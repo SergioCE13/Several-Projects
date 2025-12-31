8e93955028d76f8c4e76713af33e7cc9a482c65d
SergioCE <sergioce.ipn@gmail.com>
2025-12-30 21:29:09 -0600

    First commit to test the connection...

---
+++ First Steps/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World, ready to coding?");
+        System.out.println("Let's go! we are ready!");
+    }
+}
